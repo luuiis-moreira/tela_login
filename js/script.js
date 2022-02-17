5c1c87faa0ca769e89304ad423b6ba8387e4f03e
Luis Reinaldo Moreira <89565021+luuiis-moreira@users.noreply.github.com>
2022-02-17 15:24:50 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const imgEye = document.querySelector('#eye');
+const inputPass = document.querySelector('#pass')
+
+imgEye.addEventListener('click', () => {
+    if (inputPass.type === 'password') {
+        inputPass.type = 'text'
+        imgEye.src = '/img/hidden.png'
+    } else {
+        inputPass.type = 'password'
+        imgEye.src = '/img/eye.png'
+    }
+})
