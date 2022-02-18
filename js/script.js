71ca176fa13fa0eeefa3b8b58f8c429e0b5fdd5c
Luis Reinaldo Moreira <89565021+luuiis-moreira@users.noreply.github.com>
2022-02-18 08:40:28 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const inputPass = document.querySelector('#pass')
 imgEye.addEventListener('click', () => {
     if (inputPass.type === 'password') {
         inputPass.type = 'text'
-        imgEye.src = '/img/hidden.png'
+        imgEye.src = 'img/hidden.png'
     } else {
         inputPass.type = 'password'
-        imgEye.src = '/img/eye.png'
+        imgEye.src = 'img/eye.png'
     }
 })
